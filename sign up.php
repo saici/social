da5a4f7f3c226b3b554a13b231ffee921b3b5ce7
sabastiaan <sebaspe97@hotmail.com>
2014-10-03 09:50:11 -0700

    sign up
    regestratie bestand

---
+++ sign up.php
@@ -0,0 +1,18 @@
+<?php
+	include 'header.php';
+	
+	?>
+
+<form action="login.php" method="post">
+Username <input type="tekst" name="username"><br>
+Password <input type="tekst" name="password"><br>
+E-mail <input type="tekst" name="email"><br>
+
+<input type="submit" value="sign up!">
+
+<?php 
+	include 'footer.php';
+	?>
+	
+
+
