a76fa1b3a9db07d4cd5ebacf29e080702c55781e
Jeroen <jeroen@Jeroen-PC.(none)>
2014-10-19 14:22:01 +0200

    Uitlogen werkt

--- login.php
+++ login.php
@@ -1,10 +1,17 @@
-
+<?php 
+	include 'header.php';
+	if(isset($_GET['logout'])){
+		SESSION_DESTROY();
+		header("location: index.php");
+	}
+?>
 	<form action="includes/login.php" method="post">
-		Gebruikersnaam <input type="tekst" name="username"><br>
-		Wachtwoord: <input type="tekst" name="password"><br>
+		Gebruikersnaam <input type="text" name="username"><br>
+		Wachtwoord: <input type="password" name="password"><br>
 		
 		<input type="submit" value="Login">
 	</form>
 
 
 
+<?php include 'footer.php';?>
