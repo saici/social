f442312ccbf9d58ed823f8d7631c3898ef5745b0
jjeroennl <jjeroen@home.nl>
2014-10-20 13:27:21 +0200

    search autocomplete

--- header.php
+++ header.php
@@ -12,6 +12,28 @@ $checkpage = 'true';
 <html>
 <head>
 	<title>Social netwerk</title>
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+	<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+	<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+	<link rel="stylesheet" href="/resources/demos/style.css">
+	<script>
+
+  $(function() {
+    var availableTags = [
+      <?php 
+      $query = mysqli_query($con, "SELECT username FROM users");
+      while($row = mysqli_fetch_array($query)){
+		echo '"' . $row ['username']. '",';
+	  }
+	  ?>
+    ];
+    $( "#tags" ).autocomplete({
+      source: availableTags
+    });
+  });
+  </script>
+	</script>
 </head>
 <body>
 	<?php include 'nav.php'; ?>
+
