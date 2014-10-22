f0b3a7e7b93870939f99a9b93d8c2ba624192cc1
jjeroennl <jjeroen@home.nl>
2014-10-22 11:12:37 +0200

    Youtube fix homepagina

--- timelinejavascript.php
+++ timelinejavascript.php
@@ -1,9 +1,11 @@
 <script type="text/javascript">
      function check(){
 		var loc = document.getElementById('video').value;
-		var abc = loc.substring(loc.indexOf('v=') +1);
-		document.getElementById('youtubevideo').src = "http://www.youtube.com/embed/" + abc;
-		
+		var abc = loc.substring(loc.indexOf('v=') +2);
+		abc.replace("=", "");
+		abc = abc.split('&')[0]
+		document.getElementById('youtubevideo').src = "https://www.youtube.com/embed/" + abc;
+
 		document.getElementById('videoid').value = abc;
 		
 	
