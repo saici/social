09ef16473e5194e5125daf1db3a883cbc120e939
jjeroennl <jjeroen@home.nl>
2014-10-29 14:13:43 +0100

    foto's worden met willekeurige nummer geupload ipv id van message

--- includes/uploadimg.php
+++ includes/uploadimg.php
@@ -23,7 +23,17 @@
 			
 			$query = mysqli_query($con, "SELECT * FROM messages WHERE user = '$user' ORDER BY ID DESC LIMIT 1") or die(mysqli_error($con));
 			while($row = mysqli_fetch_array($query)){
-				$id = $row['ID'];
+				
+				$loop = 1;
+				while($loop == 1){
+					$id = rand(10000,99999);
+					$checkid = mysqli_query($con, "SELECT * FROM messages WHERE photo='$id'");
+					if(mysqli_num_rows($checkid) == 0){
+						$loop = 0;
+					}
+					else{
+						
+					}
 				$target_dir = $target_dir . '/' . $id;
 				$post = '<table><tr><td><img style="max-width: 560px; max-height: 315px;"  onclick="newimage(' . $id . ')"   data-target="#showimagemodal" data-toggle="modal"  src="http://' .  $_SERVER['SERVER_NAME'] . '/social/images/' . $id . '"></td></tr><tr><td>' . $extramessage . '</td></tr></table>';
 
