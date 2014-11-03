17f42b3e2482437897cd0c91eb4bd791c7ffb2bf
jjeroennl <jjeroen@home.nl>
2014-11-03 13:32:59 +0100

    activate

--- activate.php
+++ activate.php
@@ -2,12 +2,14 @@
 	include 'includes/connect.php';
 	if(isset($_GET['hash'])){
 		$hash = mysqli_real_escape_string($con, $_GET['hash']);
-		$checkhash = mysqli_query($con, "SELECT * FROM activate WHERE hash = '$hash'");
-		$numhash = mysqli_num_rows($con, $checkhash);
+		$checkhash = mysqli_query($con, "SELECT * FROM activate WHERE hash = '$hash'") or die(mysqli_error($con));
+		$numhash = mysqli_num_rows($checkhash);
+		
 		if($numhash == 1){
 			while($row = mysqli_fetch_array($checkhash)){
 				$user = $row['user'];
-				mysqli_query($con, "UPDATE users SET active='1' WHERE ID = '$user'");
+				mysqli_query($con, "UPDATE users SET active='1' WHERE ID = '$user'") or die(mysqli_error($con));
+				mysqli_query($con, "DELETE FROM activate WHERE hash = '$hash'") or die(mysqli_error($con));
 				headers("Location: index.php");
 			}
 		}
