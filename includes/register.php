78e8630a91f26c29145980281490b0cca7a5c00c
jjeroennl <jjeroen@home.nl>
2014-11-28 19:54:08 +0100

    register

--- includes/register.php
+++ includes/register.php
@@ -24,16 +24,16 @@ $password = salt($username, $password, $email, $date);
 //insert in database
 mysqli_query($con, "INSERT INTO users(username, password, registrationdate, email, active, ID) VALUES('$username', '$password',NOW(), '$email', '1', '$id')") or die(mysqli_error($con));
 mysqli_query($con, "INSERT INTO profile(user, avatar, bio, banner, ID) VALUES('$id', 'NONESET', 'NONESET', 'NONESET', '$id')") or die(mysqli_error($con));
-header("Location: login.php?register");
+header("Location: ../index.php?register");
 }
 else{
 //Mail already exist in database
-header("Location: login.php?emailfail");
+header("Location: ../login.php?emailfail");
 die('Nomail');
 }
 }
 else{
 //fail or nothing inserted in form
-header("Location: login.php?fail");
+header("Location: ../login.php?fail");
 }
 ?>
